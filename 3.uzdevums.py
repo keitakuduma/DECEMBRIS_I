3240f566d144ace772e8fb27c9961f6756ee75de
keitakuduma <keita.kuduma@grobinasskola.lv>
2023-11-24 10:18:02 +0200

    https://github.com/keitakuduma/DECEMBRIS_I.git

---
+++ 3.uzdevums.py
@@ -0,0 +1,6 @@
+print("Cipari kuri dalās gan ar 3 gan ar 7 skaitļu grupā no 1 līdz 100:")
+for i in range (1, 100):
+    if (i % 3 == 0 and i % 7 == 0):
+        print (i)
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DECEMBRIS_I
+# Manuprāt, man izdevās vis ar GitHub, taču python uzdotie uzdevumi sagādāja problēmas.
